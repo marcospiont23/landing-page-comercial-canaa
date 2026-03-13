8854cc4b8e162e434b0d6d18c08dd54d38f47eec
marcospiont23 <marcosvinicius_lp@hotmail.com>
2026-03-12 21:28:38 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          green: '#00A859',
+          'green-dark': '#008445',
+          blue: '#0066CC',
+          'blue-dark': '#004C99',
+          dark: '#1A202C'
+        }
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      }
+    }
+  }
+}
